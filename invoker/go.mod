0b27bb4cce18296770be6a0a2c8042fccbf50eb5
Rost Khaniukov <khaniukov.rostyslav@pdffiller.team>
2022-03-22 19:52:24 +0200

    Extend

---
+++ invoker/go.mod
@@ -0,0 +1,9 @@
+module lambda-invoker
+
+go 1.17
+
+require github.com/aws/aws-lambda-go v1.28.0
+
+require github.com/aws/aws-sdk-go v1.43.22
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
---
+++ invoker/invoker.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	lambdaService "github.com/aws/aws-sdk-go/service/lambda"
+)
+
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
+func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	client := lambdaService.New(sess, &aws.Config{Region: aws.String("us-east-1")})
+	result, err := client.Invoke(&lambdaService.InvokeInput{
+		FunctionName: aws.String("MyGetItemsFunction"),
+	})
+	if err != nil {
+		return fmt.Sprintf("Error calling MyGetItemsFunction"), nil
+	}
+
+	return fmt.Sprintf("All OK! %s\n", result), nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
