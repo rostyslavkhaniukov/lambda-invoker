f7d5499cb410e3f1f517aecf3e33a3fbda464c1b
Rost Khaniukov <khaniukov.rostyslav@pdffiller.team>
2022-03-22 19:25:57 +0200

    Extend

---
+++ terraform/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+locals {
+  web_lambda_name = format("%s-web", var.lambda_function_name)
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.environment_name}-role-addons"
+
+  assume_role_policy = file(format("%s/templates/assume-role-policy.json", path.module))
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name   = "lambda-policy-addons"
+  policy = templatefile(format("%s/templates/role-policy.json.tmpl", path.module), {})
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+resource "aws_lambda_function" "web_lambda" {
+  function_name = local.web_lambda_name
+  package_type  = "Image"
+  image_uri     = "499781533590.dkr.ecr.us-east-1.amazonaws.com/lambda-invoker:master_87d517"
+  
+  role = aws_iam_role.lambda_role.arn
+
+  memory_size = 1024
+  timeout     = 28
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "environment_name" {
+  type = string
+}
+
+variable "lambda_function_name" {
+  type = string
+}
