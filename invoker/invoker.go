125b3d8259f00ae2d55a00f6bcd3be77f3d1773e
Rost Khaniukov <khaniukov.rostyslav@pdffiller.team>
2022-03-30 18:52:17 +0300

    Fix

--- invoker/invoker.go
+++ invoker/invoker.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	lambdaService "github.com/aws/aws-sdk-go/service/lambda"
-	"strings"
 )
 
 type InvokerEvent struct {
 	LambdaName string `json:"lambda_name"`
-	Arguments string `json:"arguments"`
+	Arguments  string `json:"arguments"`
 }
 
 func HandleRequest(ctx context.Context, event InvokerEvent) (string, error) {
@@ -22,34 +22,32 @@ func HandleRequest(ctx context.Context, event InvokerEvent) (string, error) {
 
 	client := lambdaService.New(sess, &aws.Config{Region: aws.String("us-east-1")})
 
-	output, err := client.ListFunctions(&lambdaService.ListFunctionsInput{
+	/*output, err := client.ListFunctions(&lambdaService.ListFunctionsInput{
 		FunctionVersion: aws.String("ALL"),
 	})
 	if err != nil {
 		return fmt.Sprintf("ListFunctions. Error: %s", err.Error()), nil
 	}
 
-	fmt.Printf("Fetched: %d", len(output.Functions))
-
 	var functions []string
 	for _, elem := range output.Functions {
 		functions = append(functions, *elem.FunctionName)
+	}*/
+
+	payload, err := json.Marshal([]byte(event.Arguments))
+	if err != nil {
+		return fmt.Sprintf("Invalid CLI arguments. Error: %s", err.Error()), nil
 	}
 
-	//payload, err := json.Marshal([]byte(event.Arguments))
-	//if err != nil {
-	//	return fmt.Sprintf("Invalid CLI arguments. Error: %s", err.Error()), nil
-	//}
-
-	/*result, err := client.Invoke(&lambdaService.InvokeInput{
+	result, err := client.Invoke(&lambdaService.InvokeInput{
 		FunctionName: aws.String(event.LambdaName),
-		Payload: payload,
+		Payload:      payload,
 	})
 	if err != nil {
 		return fmt.Sprintf("Error calling %s. Error: %s", event.LambdaName, err.Error()), nil
-	}*/
+	}
 
-	return fmt.Sprintf("Done! %s\n", strings.Join(functions, ",")), nil
+	return fmt.Sprintf("Done! %s\n", result), nil
 }
 
 func main() {
